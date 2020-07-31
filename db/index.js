59158726ba641fe7d57e027ebb8479956738cc97
Mackenzie Clark <clark.rmackenzie@gmail.com>
2020-07-31 01:27:49 -0400

    begining of index.js with DB class, connection

---
+++ db/index.js
@@ -0,0 +1,16 @@
+// Functions for performing specific SQL queries
+// Using class and constructor function
+
+// Set Requirements
+const connection = require("./connection");
+
+// Create a class to be called on 
+class DB {
+
+    // Reference connection using CONSTRUCTOR
+    constructor(connection){
+        this.connection = connection;
+    }
+
+    // Begin Functions
+} // End Functions
