7fd22c78152d08fb244f9f9ca49eef44e28b3df0
Mackenzie Clark <clark.rmackenzie@gmail.com>
2020-07-31 01:43:20 -0400

    initialize application, attempt logo

--- index.js
+++ index.js
@@ -2,3 +2,16 @@
 const { prompt } = require("inquirer");
 const db = require("./db");
 require("console.table");
+
+// Initialize Application
+function init() {
+    const logoText = render({ name: "Employee CLI System" });
+
+    console.log(logoText);
+
+
+}
+
+
+// Begin!
+init();
