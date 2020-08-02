9fae18b9d1dfe127a61a20a3cd4a0ac9f6e661c6
Mackenzie Clark <clark.rmackenzie@gmail.com>
2020-08-02 16:32:43 -0400

    schema (final) update

--- db/schema.sql
+++ db/schema.sql
@@ -14,11 +14,14 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(50) NOT NULL,
-    salary DECIMAL NOT NULL
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL
 );
 
 CREATE TABLE employee (
     id INT AUTO_INCREMENT PRIMARY KEY,
     firt_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL
+    last_name VARCHAR(50) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL
 );
