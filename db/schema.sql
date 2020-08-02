fea6e03ff099e67473b69a03ee0f4a4db9956387
Mackenzie Clark <clark.rmackenzie@gmail.com>
2020-08-02 16:29:40 -0400

    schema update

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,24 @@
--- Create a new database
+-- Set up a new database
 
 DROP DATABASE IF EXISTS employees;
 CREATE DATABASE employees;
+USE employees;
+
+-- Create tables for three main categories
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    salary DECIMAL NOT NULL
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    firt_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL
+);
