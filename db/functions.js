e74917557205da7d5f2e40b6e02a5fb69d9cfec8
Mackenzie Clark <clark.rmackenzie@gmail.com>
2020-08-02 17:46:39 -0400

    join for employee

--- db/functions.js
+++ db/functions.js
@@ -13,4 +13,14 @@ class DB {
     }
 
     // Begin Functions
+
+    // All Employees Function
+    // This will JOIN roles and departments to display ROLE, SALARY, DEPARTMENT, and MANAGER
+    allEmployees() {
+        return this.connection.query(
+            "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
+        );
+    }
+
+
 } // End Functions
